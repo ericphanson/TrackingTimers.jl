c50e2d137b05719133f7c049affce4c58fc4f29d
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-02-28 20:08:24 +0100

    initial functionality

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using TrackingTimers
 using Test
+using Aqua
 
 @testset "TrackingTimers.jl" begin
     # Write your tests here.
+end
+
+@testset "Aqua tests" begin
+    Aqua.test_all(TrackingTimers; ambiguities=false)
 end
