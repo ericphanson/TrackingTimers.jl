9af685ef240ff72c0e2353dd1412c889c9ee4b32
Phillip Alday <phillip.alday@beacon.bio>
2022-10-18 11:58:32 -0500

    update format check CI

--- format/run.jl
+++ format/run.jl
@@ -1,7 +1,7 @@
 using JuliaFormatter
 
 function main()
-    perfect = format(joinpath(@__DIR__, ".."); style=YASStyle(), verbose=true)
+    perfect = format(joinpath(@__DIR__, ".."), YASStyle(); verbose=true)
     if perfect
         @info "Linting complete - no files altered"
     else
